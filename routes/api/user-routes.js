36c0e17a39174bea5e44d119d469216af29a77f3
Paul Barcenas <paulbarcenas@yahoo.com>
2021-10-26 22:53:13 -0500

    create the login route for authentication

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -50,7 +50,28 @@ router.post('/', (req,res) =>{
     res.status(500).json(err);
   });
 });
-
+router.post('/login', (req,res) => {
+  // Query operation
+  // expects {email: 'lernatino@gmail.com', password: 'password1234'}
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  }).then(dbUserData => {
+    if(!dbUserData){
+      res.status(400).json({message: 'No user with that email adress!' });
+      return;
+    }
+    //res.json({ user: dbUserData});
+    // Verify user
+    const validPassword = dbUserData.checkPassword(req.body.password);
+    if(!validPassword) {
+      res.status(400).json({ message: 'Incorrect password!' });
+      return;
+    }
+    res.json({ user: dbUserData, message: 'You are now logged in!' });
+  });
+});
 // PUT /api/users/1
 router.put('/:id', (req,res)=>{
   // expects {username: 'Lernantino', email: lernantion@gmail.com', password:'password1234'}
