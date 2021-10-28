2e1aa94c7475af38530f24faf18eac70a23a1c24
Paul Barcenas <paulbarcenas@yahoo.com>
2021-10-27 22:25:44 -0500

    update GET routes to include Votes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models');
-
+const { User, Post, Vote } = require("../../models");
 
 // GET /api/users
 router.get('/', (req,res) => {
@@ -19,6 +18,18 @@ User.findAll({
 router.get('/:id', (req,res) => {
   User.findOne({
     attributes: { exclude: ['password']},
+    include: [
+      {
+        model: Post,
+        attributes: ['id', 'title', 'post_url', 'created_at']
+      },
+      {
+        model: Post,
+        attributes: ['title'],
+        through: Vote,
+        as: 'voted_posts'
+      }
+    ],
     where: {
       id: req.params.id
     }
