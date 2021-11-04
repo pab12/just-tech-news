fddd469544c4a22e24da9b4f495764f58782ad20
Paul Barcenas <paulbarcenas@yahoo.com>
2021-11-03 19:19:42 -0500

    Create a login and signup page also being able to destroy the session

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -63,11 +63,15 @@ router.post('/', (req,res) =>{
     email: req.body.email,
     password: req.body.password
   })
-  .then(dbUserData => res.json(dbUserData))
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  });
+  .then(dbUserData => {
+    req.session.save(() => {
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+  
+      res.json(dbUserData);
+    });
+  })
 });
 router.post('/login', (req,res) => {
   // Query operation
@@ -88,9 +92,30 @@ router.post('/login', (req,res) => {
       res.status(400).json({ message: 'Incorrect password!' });
       return;
     }
-    res.json({ user: dbUserData, message: 'You are now logged in!' });
+    req.session.save(() => {
+      // declare session variables
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+
+      res.json({ user: dbUserData, message: 'You are now logged in!' });
+    });
   });
 });
+
+//logout
+router.post('/logout', (req,res)=> {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  }
+  else {
+    res.status(404).end();
+  }
+});
+
+
 // PUT /api/users/1
 router.put('/:id', (req,res)=>{
   // expects {username: 'Lernantino', email: lernantion@gmail.com', password:'password1234'}
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,6 +4,8 @@ const {Post, User, Comment } = require('../models');
 
 
 router.get('/', (req, res) => {
+  console.log(req.session);
+
   Post.findAll({
     attributes: [
       'id',
@@ -41,6 +43,10 @@ router.get('/', (req, res) => {
 
 
 router.get('/login',(req,res)=> {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
   res.render('login');
 });
 
